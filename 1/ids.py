94082f32ae562c601169e01f4267ab1e50a54a4c
Manish-Hazra <iammanishhazra@gmail.com>
2022-03-02 15:21:24 +0530

    Added Simulated Annealing

--- 1/ids.py
+++ 1/ids.py
@@ -1,50 +1,33 @@
-# Python program to print DFS traversal from a given
-# given graph
 from collections import defaultdict
-
-# This class represents a directed graph using adjacency
-# list representation
 class Graph:
 
 	def __init__(self,vertices):
 
-		# No. of vertices
 		self.V = vertices
 
-		# default dictionary to store graph
 		self.graph = defaultdict(list)
 
-	# function to add an edge to graph
 	def addEdge(self,u,v):
 		self.graph[u].append(v)
 
-	# A function to perform a Depth-Limited search
-	# from given source 'src'
+	# A function to perform a Depth-Limited search from given source 'src'
 	def DLS(self,src,target,maxDepth):
 
 		if src == target : return True
-
-		# If reached the maximum depth, stop recursing.
 		if maxDepth <= 0 : return False
 
-		# Recur for all the vertices adjacent to this vertex
 		for i in self.graph[src]:
 				if(self.DLS(i,target,maxDepth-1)):
 					return True
 		return False
 
-	# IDS to search if target is reachable from v.
-	# It uses recursive DLS()
 	def IDDFS(self,src, target, maxDepth):
 
-		# Repeatedly depth-limit search till the
-		# maximum depth
 		for i in range(maxDepth):
 			if (self.DLS(src, target, i)):
 				return True
 		return False
 
-# Create a graph
 g = Graph (7)
 g.addEdge(0, 1)
 g.addEdge(0, 2)
--- 4/saa.py
+++ 4/saa.py
@@ -1,40 +1,51 @@
-import random
-import math
+from numpy import asarray, exp
+from numpy.random import randn, rand, seed
+from matplotlib import pyplot
 
-def simulated_annealing(initial_state):
-    """Peforms simulated annealing to find a solution"""
-    initial_temp = 90
-    final_temp = .1
-    alpha = 0.01
-    
-    current_temp = initial_temp
+# Define objective function
+def objective(step):
+    return step[0] ** 2.0
 
-    # Start by initializing the current state with the initial state
-    current_state = initial_state
-    solution = current_state
+# Define simulated annealing algorithm
+def sa(objective, area, iterations, step_size, temperature):
+    # create initial point
+    start_point = area[:, 0] + rand( len( area ) ) * ( area[:, 1] - area[:, 0] )
+    # evaluate initial point
+    start_point_eval = objective(start_point)
+    # Assign previous and new solution to previous and new_point_eval variable 
+    mia_start_point, mia_start_eval = start_point, start_point_eval
+    outputs = []
+    for i in range(iterations):
+        # First step by mia
+        mia_step = mia_start_point + randn( len( area ) ) * step_size  
+        mia_step_eval = objective(mia_step)
+        if mia_step_eval < start_point_eval:
+            start_point, start_point_eval = mia_step, mia_step_eval
+      #Append the new values into the output list
+            outputs.append(start_point_eval)
+            print('Acceptance Criteria = %.5f' % mac," ",'iteration Number = ',i," ", 'best_so_far = ',start_point," " ,'new_best = %.5f' % start_point_eval)
+        difference = mia_step_eval - mia_start_eval
+        t = temperature / float(i + 1)
+        # calculate Metropolis Acceptance Criterion / Acceptance Probability
+        mac = exp(-difference / t)
+        # check whether the new point is acceptable 
+        if difference < 0 or rand() < mac:
+            mia_start_point, mia_start_eval = mia_step, mia_step_eval
+    return [start_point, start_point_eval, outputs]
 
-    while current_temp > final_temp:
-        neighbor = random.choice(get_neighbors())
-
-        # Check if neighbor is best so far
-        cost_diff = get_cost(current_state) - get_cost(neighbor)
-
-        # if the new solution is better, accept it
-        if cost_diff > 0:
-            solution = neighbor
-        # if the new solution is not better, accept it with a probability of e^(-cost/temp)
-        else:
-            if random.uniform(0, 1) < math.exp(-cost_diff / current_temp):
-                solution = neighbor
-        # decrement the temperature
-        current_temp -= alpha
-
-    return solution
-
-def get_cost(state):
-    """Calculates cost of the argument state for your solution."""
-    raise NotImplementedError
-    
-def get_neighbors(state):
-    """Returns neighbors of the argument state for your solution."""
-    raise NotImplementedError
+seed(1)
+# define the area of the search space
+area = asarray([[-6.0, 6.0]])
+# initial temperature
+temperature = 12
+# define the total no. of iterations
+iterations = 1200
+# define maximum step_size
+step_size = 0.1
+# perform the simulated annealing search
+start_point, output, outputs = sa(objective, area, iterations, step_size, temperature)
+#plotting the values
+pyplot.plot(outputs, 'ro-')
+pyplot.xlabel('Improvement Value')
+pyplot.ylabel('Evaluation of Objective Function')
+pyplot.show()
